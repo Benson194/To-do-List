85df2278d0e71f3df284f20d2ef74c85f5250939
Benson Thew <bensonthew@alphareds.net>
2021-08-19 22:55:26 +0800

    Organize imports

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,13 +1,3 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-
-import 'package:to_do_list/main.dart';
-import 'package:to_do_list/screens/home_screen/home_screen_bloc.dart';
-import 'package:to_do_list/screens/home_screen/home_screen_event.dart';
-import 'package:to_do_list/screens/home_screen/home_screen_state.dart';
-
 import 'helper/date_time_helper_test.dart';
 import 'screens/home_screen/home_screen_test.dart';
 
